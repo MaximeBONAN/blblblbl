d99c37dbce1ab1ee222de132a95ed5d2ccf283f5
barrebi <bonanmaxime@outlook.fr>
2015-03-04 15:17:24 +0100

    Update main.cpp

--- Mini_-_Projet_tAaDG/main.cpp
+++ Mini_-_Projet_tAaDG/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include "CasePlateau.h"
+#include "Joueurs.h"
+#include "Partie.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    Plateau plateaucaca =  Plateau(10);
+    plateaucaca.affiche();
     return 0;
 }
